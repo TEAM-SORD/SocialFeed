ea815295ad5d5634cf232b5187f42fd2e8561cf7
sarah <sarahabimay@gmail.com>
2015-02-17 13:02:44 +0000

    20150217.4 simplified test file

--- test.js
+++ test.js
@@ -1,29 +1,9 @@
 var test = require( 'tape');
 var script = require( './script.js' );
 
-test( 'result to html', function( t) {
-	var exampleInsta = {
-		user : 'username',
-		comment: 'comment',
-		media_url: 'picture'
-	};
-	var expectedHTML = '<div> <p> ' + exampleInsta.user +'</p>' + 
-							 '<img src=' + exampleInsta.media_url + '>' +
-							 '<p>' + exampleInsta.comment + '</p>' +
-						 '</div>';
+test( 'easy pass test', function( t) {
+	
 
 	t.plan( 1 );
-	t.equal( script.formatInstaForHTML( tweet ), expectedHTML );
+	t.equal( 1,2 );
 });
-
-test( 'extract data from instagram response', function (t) {
-
-	var instaResponse = {};
-	var extractedData =  {
-		user : 'username',
-		comment: 'comment',
-		media_url: 'picture'
-	};
-	t.plan(1);
-	t.equal( extractDataFromResponse( instaResponse ), extractedData );
-}
