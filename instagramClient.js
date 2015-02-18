50d46b4b78139c8bf0ea13e120686e03df9a1427
sarah <sarahabimay@gmail.com>
2015-02-18 19:38:26 +0000

    trying to get env vars working again

--- instagramClient.js
+++ instagramClient.js
@@ -4,14 +4,16 @@ var url = require( 'url');
 var ig = require('instagram-node').instagram();
 var http = require('http');
 
-var environment,envVars;
+var twitEnv, instaEnv;
 try{
 	var apiConfig = require( './config.json');
-	envVars= {
+	twitEnv = {
 	  consumer_key: apiConfig.cKey,
 	  consumer_secret: apiConfig.cSecret,
 	  access_token_key: apiConfig.atKey,
 	  access_token_secret: apiConfig.atSecret,
+  	};
+  	instaEnv = {
 	  client_id: apiConfig.instaCID,
 	  client_secret: apiConfig.instaCSecret
 	};
@@ -19,30 +21,33 @@ try{
 catch( err ){
 	console.log( 'Error getting apiConfig, try Heroku Env Vars: ' + err );
 	// cannot find config.json so try for Heroku Env Vars instead
- 	envVars = {
+ 	twitEnv = {
 	  consumer_key: process.env.TWIT_C_KEY,
 	  consumer_secret: process.env.TWIT_C_SECRET,
 	  access_token_key: process.env.TWIT_AT_KEY,
 	  access_token_secret: process.env.TWIT_AT_SECRET,
+  	};
+
+  	instaEnv = {
 	  client_id: process.env.INSTA_CID,
 	  client_secret: process.env.INSTA_CSECRET
 
 	};
-	console.log( 'ENV VAR CKEY: ' + envVars.consumer_key );
-	console.log( 'ENV VAR CSecret: ' + envVars.consumer_secret );
-	console.log( 'ENV VAR atKey: ' + envVars.access_token_key );
-	console.log( 'ENV VAR atSecret: ' + envVars.access_token_secret );
-	console.log( 'ENV VAR INSTA Client ID: ' + envVars.client_id );
-	console.log( 'ENV VAR INSTA Client Secret: ' + envVars.client_secret );
+	console.log( 'ENV VAR CKEY: ' + twitEnv.consumer_key );
+	console.log( 'ENV VAR CSecret: ' + twitEnv.consumer_secret );
+	console.log( 'ENV VAR atKey: ' + twitEnv.access_token_key );
+	console.log( 'ENV VAR atSecret: ' + twitEnv.access_token_secret );
+	console.log( 'ENV VAR INSTA Client ID: ' + instaEnv.client_id );
+	console.log( 'ENV VAR INSTA Client Secret: ' + instaEnv.client_secret );
 };
 // if Heroku Env Vars weren't set then client will be nothing
-if( envVars.consumer_key === undefined || envVars.client_id === undefined){
-	console.log( 'envVars undefined');
+if( twitEnv.consumer_key === undefined || instaEnv.client_id === undefined){
+	console.log( 'Environment Vars Undefined');
 }
 else {
-	client = new Twitter( envVars );
+	client = new Twitter( twitEnv );
 	//instagram
-	ig.use( envVars );
+	ig.use( instaEnv );
 }
 
 // try{
@@ -89,24 +94,33 @@ module.exports = {
 		module.exports.queryHandler( reqURL.query.query, response, callback );
 	},
 	queryHandler: function( query, response, callback){
-		if( envVars === undefined ){
+		if( twitEnv === undefined || instaEnv === undefined ){
 	    	console.log( 'No Twitter Client object. Issue setting Auth Keys.')
 	    	response.writeHead( 200, {'Content-Type': 'text/plain'});
 	    	response.end( 'Undefined Twitter or instagram client object');
 	    }
 	    else {	
 	    	console.log( "In function queryHandler");
-	    	ig.tag_media_recent( /*reqURL.query.*/query , function(err, medias, pagination, remaining, limit) {	    		
-		    	console.log( "In callback of tag_media_recent");
-				var responseData = module.exports.extractDataFromResponse( medias );
-				console.log(err);
-				if(err) throw err;
-				response.writeHead( 200, {'Content-Type': 'text/plain'});
-				response.end(responseData);
-				if( callback !== undefined ) {
-					callback( response );
-				};
-			});
+	    	// make a call to the database and see if there is any data for our query 
+	    	// var data = {};
+	    	// data = lookupQuery( query );
+	    	// if( data.isFresh() ) { 
+	    	// 	response.end( data );
+	    	// }
+	    	// else {
+	    		ig.tag_media_recent( /*reqURL.query.*/query , function(err, medias, pagination, remaining, limit) {	    		
+			    	console.log( "In callback of tag_media_recent");
+			    	// 
+					var responseData = module.exports.extractDataFromResponse( medias );
+					console.log(err);
+					if(err) throw err;
+					response.writeHead( 200, {'Content-Type': 'text/plain'});
+					response.end(responseData);
+					if( callback !== undefined ) {
+						callback( response );
+					};
+				});
+			// };
 		};
 	},
 
