99dddd9c7c82d109def1ae21146f61150a3e525c
sarah <sarahabimay@gmail.com>
2015-02-19 23:59:18 +0000

    database code and rubbish test

--- instagramClient.js
+++ instagramClient.js
@@ -6,7 +6,7 @@ var moment = require('moment');
 var database = 'mongodb://localhost:27017/instagramDB';
 var mongojs = require('mongojs');
 var db = mongojs(database, ['MediaResults']);
-
+var STALE_TIME_LIMIT = 2;// 2 minutes
 db.open();
 //console.log( db );
 var twitEnv, instaEnv;
@@ -68,7 +68,7 @@ module.exports = {
 	extractJSONFromResponseStringified: function (medias){
 		var tagsData;
 		if( medias === undefined ) {
-			config.log( 'No Media found');
+			console.log( 'No Media found');
 			return tagsData;
 		};
 		tagsData = JSON.stringify( medias.map( function (media) {
@@ -103,20 +103,21 @@ module.exports = {
 		module.exports.queryHandler( reqURL.query.query, response, callback );
 	},
 	queryHandler: function( query, response, callback){
-	    	var data = {
-	    		results: [],
-	    		isFresh: function() {
-	    			console.log( 'In isFresh(), Data.Results: ' + this.results )
-	    			if( this.results === undefined || 
-	    				this.results.length === 0  || 
-	    				isDocumentStale( this.results[0].dateTimeInserted ) === true ){
-	    				console.log( "Documents are stale ");
-	    				return false;
-	    			};
-	    			console.log( "Documents aren't stale yet.");
-	    			return true;
-	    		}
-	    	};
+		var docs, documents;
+    	var data = {
+    		results: [],
+    		isFresh: function() {
+    			console.log( 'In isFresh()' )
+    			if( this.results === undefined || 
+    				this.results.length === 0  || 
+    				module.exports.isDocumentStale( this.results[0].dateTimeInserted ) === true ){
+    				console.log( "Documents are stale ");
+    				return false;
+    			};
+    			console.log( "Documents aren't stale yet.");
+    			return true;
+    		}
+    	};
 		if( twitEnv === undefined || instaEnv === undefined ){
 	    	console.log( 'No Twitter Client object. Issue setting Auth Keys.');
 	    	response.writeHead( 200, {'Content-Type': 'text/plain'});
@@ -126,55 +127,70 @@ module.exports = {
 	    	console.log( "In function queryHandler");
 	    	// make a call to the database and see if there is any data for our query 
 	    	var dbError, formattedData;
-	    	data.results = module.exports.lookupQueryInDB( query );
-	    	if( data.isFresh() ) { 
-	    		formattedData = module.exports.formatDocumentResponse( data );
-	    		console.log( 'Collection data formatted for response to browser' + formattedData );
-	    		response.end( formattedData );
-	    	}
-	    	else {
-	    		ig.tag_media_recent( /*reqURL.query.*/query , function(err, medias, pagination, remaining, limit) {	    		
-			    	console.log( "In callback of tag_media_recent");			    	
-					var responseData = module.exports.extractJSONFromResponseStringified( medias );
-					// add extracted data to database and then return is on the response
-					try{
-						// if there's data in the database then it will need to be replaced
-						var docs = module.exports.lookupQueryInDB( 'MediaResults', query );
-						if( docs.length > 0 ) {
-							if( docs.length !== module.exports.removeDocsFromCollection( query ).WriteResult ) {
-								console.log( 'Not all of the docs were removed!');
-							}
-							else{
-								console.log( 'All docs have been removed!' );
-							};
-						};
-						// and then new data added
-						data = addToCollection( 'MediaResults', responseData, query );
-						if( data === undefined ) {
-							console.log( 'Error adding data to database: ' );
-						};
-					}
-					catch( error ){
-						console.log( 'I think the add to collection failed: Error: ' + error );
-					}
-					if(err) {
-						console.log( "In tag_media_recent callback. Error: " + err);
-						throw err;
+	    	var projection = { 'dateTimeInserted': 1, 'username': 1, 'link':1, 'caption':1, 'image':1, _id: 0 };
+	    	module.exports.lookupQueryInDB( query, projection, function( documents ) { 
+	    		data.results = documents;
+		    	if( data.isFresh() ) { 
+		    		//formattedData = module.exports.formatDocumentResponse( documents );
+		    		//console.log( 'Collection data formatted for response to browser' + JSON.stringify( documents ) );
+		    		response.end( JSON.stringify( documents ));
+		    		if( callback !== undefined ) {
+						callback( JSON.stringify( documents ) );
 					};
-					response.writeHead( 200, {'Content-Type': 'text/plain'});
-					response.end(responseData);
-					if( callback !== undefined ) {
-						callback( response );
-					};
-				});
-			};
+		    	}
+		    	else {
+		    		ig.tag_media_recent( /*reqURL.query.*/query , function(err, medias, pagination, remaining, limit) {	    		
+				    	console.log( "In callback of tag_media_recent");			    	
+						var responseData = module.exports.extractJSONFromResponseStringified( medias );
+						
+						if(err) {
+							console.log( "In tag_media_recent callback. Error: " + err);
+							throw err;
+						};
+						response.writeHead( 200, {'Content-Type': 'text/plain'});
+						response.end(responseData);
+						if( callback !== undefined ) {
+							callback( responseData );
+						};
+						// add extracted data to database and then return is on the response
+						try{
+							// if there's data in the database then it will need to be replaced
+							module.exports.lookupQueryInDB( query, {}, function( docs ){ 
+								if( docs && docs.length > 0 ) {
+									module.exports.removeDocsFromCollection( docs, query, function( nRemoved ) {
+										if( docs.length !== nRemoved ) {
+											console.log( 'Not all of the docs were removed!');
+										}
+										else{
+											console.log( 'All docs have been removed!' );
+										};
+										// and then new data added
+										module.exports.addToCollection( responseData, query );
+										if( callback !== undefined ) {
+											callback( response );
+										};
+									});
+								}
+								else{
+									console.logs( 'No documents found to remove');
+								};
+							});
+						}
+						catch( error ){
+							console.log( 'I think the add to collection failed: Error: ' + error );
+						}
+						
+					});
+				};
+			});
 		};
 	},
 	isDocumentStale: function( entryDateTime ) {
 		//var startDate = moment('2013-5-11 8:73:18', 'YYYY-M-DD HH:mm:ss')
+		console.log( 'entryDateTime: ' + entryDateTime);
 		var timeNow = moment(moment(), 'YYYY-M-DD HH:mm:ss');
 		var minutesDiff = timeNow.diff( entryDateTime, 'minutes');
-		console.log(minutesDiff);
+		console.log('isDocumentStale - Difference in time: ' + minutesDiff);
 		return ( minutesDiff > STALE_TIME_LIMIT )? true : false;
 	},
 	createDatabaseCollection: function( ){
@@ -183,26 +199,45 @@ module.exports = {
 			console.log( 'In collection constructor cb');
 		} );		
 	},
-	lookupQueryInDB: function( collectionName, query ) {
-		if( !DATABASE_CONNECTION ){
-			console.log( 'In lookupQueryInDB - Database Not Connected!');
-			return;
-		};
-		db.MediaResults.find( { queriedTerm: query }, function(err, docs) {
+	lookupQueryInDB: function( query, projection, callback ) {
+		var err, docs;
+		// if( !DATABASE_CONNECTION ){
+		// 	console.log( 'In lookupQueryInDB - Database Not Connected!');
+		// 	return;
+		// };
+		console.log( 'In lookupQueryInDB');
+		db.MediaResults.find( { queriedTerm: query }, projection, function(err, docs) {
     		// docs is an array of all the documents in mycollection
     		if( err ){
     			console.log( err );
-    		}
-    		console.log( 'Any Documents found?: ' + docs)
-    		return docs;
+    		};
+    		console.log( ( docs && docs.length > 0 ) ? 'Any Documents found?: ' + docs.length : 'No Documents found.');
+			callback( docs ); 
 		});
 	},
-	removeDocsFromCollection: function( query ) {
-		var writeResult = db.MediaResults.find({ queriedTerm: query}).remove();
-		console.log( "In removeDocsFromCollection, number removed: " + writeResult.nRemoved );
-		return writeResult;
+	removeDocsFromCollection: function( docs, query , callback ) {
+		var writeResult = 0;
+		var documents, err, nRemoved;
+		console.log( 'In removeDocsFromCollection');
+		if( docs && docs.length > 0 ) {
+			db.MediaResults.remove( {queriedTerm: query }, function( err, writeResult ) {
+			//db.MediaResults.find({ queriedTerm: query}, function( err, documents ) {
+				if( err ){
+    				throw err;
+    			};
+
+    			console.log( "Result from remove: " + writeResult.nRemoved );
+				//writeResult = documents.remove();
+				console.log( "In removeDocsFromCollection, number removed: " + writeResult.nRemoved );
+				callback( writeResult.nRemoved );
+			})
+		}
+		else {
+			callback( writeResult );
+		};
 	},
-	formatDocumentResponse: function( data ) {
+	formatDocumentResponse: function( documents ) {
+		// 
 		// return a JSON string of data
 		return { 'username': data.results.username,
 				 'link'    : data.results.link,
@@ -210,16 +245,16 @@ module.exports = {
 				 'image'   : data.results.imageURL
 				};
 	},
-	addToCollection: function( collectionName, responseData, query ) {
-		if( !DATABASE_CONNECTION ) {
-			console.log( "In addToCollection - Database Not Connected!");
-			return;
-		};
+	addToCollection: function( responseData, query ) {
+		// if( !DATABASE_CONNECTION ) {
+		// 	console.log( "In addToCollection - Database Not Connected!");
+		// 	return;
+		// };
 		var apiResponse = JSON.parse( responseData );
 		var tobeinserted = apiResponse.map( function( element, index ) {
 			element.queriedTerm = query;
-			var timeNow = moment(moment(), 'YYYY-M-DD HH:mm:ss');
-			console.log( 'timeNow: ' + timeNow);
+			//var timeNow = moment(moment(), 'YYYY-M-DD HH:mm:ss');
+			//console.log( 'timeNow: ' + timeNow);
 			element.dateTimeInserted = moment(moment(), 'YYYY-M-DD HH:mm:ss');
 			return element;
 		});
@@ -227,17 +262,13 @@ module.exports = {
 		
 		// do a bulk insert
 		var bulk = db.MediaResults.initializeOrderedBulkOp();
-		// for( var i = 0; i< tobeinserted.length; ++i ){
-		// 	console.log( 'Before insert index[' + i + '] of: ' + tobeinserted[i].username );
-		// 	bulk.insert(element);
-		// }
 		tobeinserted.forEach( function( element, index ) {
-			console.log( 'Before insert index[' + index + '] of: ' + element.queriedTerm );
-			console.log( 'Link: ' + element.dateTimeInserted );
-			console.log( 'Link: ' + element.link );
-			console.log( 'Link: ' + element.caption );
-			console.log( 'Link: ' + element.image );
-			console.log( 'Link: ' + element.username);
+			// console.log( 'Before insert index[' + index + '] of: ' + element.queriedTerm );
+			// console.log( 'dateTimeInserted: ' + element.dateTimeInserted );
+			// console.log( 'Link: ' + element.link );
+			// console.log( 'caption: ' + element.caption );
+			// console.log( 'image: ' + element.image );
+			// console.log( 'Username: ' + element.username);
 			bulk.insert(element);
 		});
 		bulk.execute( function( err, res ) {
@@ -245,17 +276,9 @@ module.exports = {
 			if( err ) {
 				console.log( 'Error: ' + err )
 			};
-			console.log( res );
+//			console.log( res );
 			return res;
 		});
-		// db.MediaResults.insert( JSON.stringify( apiResponse ), function( err, docs ){
-		// 	console.log( 'In Insert CB');
-		// 	if( err ) {
-		// 		console.log( err )
-		// 	};
-		// 	console.log( docs );
-		// 	return docs;
-		// });
 	},
 	handleMedia: function(err, medias, pagination, remaining, limit, response, callback) {	    		
     	console.log( "In function queryHandler");
